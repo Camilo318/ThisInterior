187aab866cdbd07d6a7fa27b44eec3bbb0a5c0bc
Camilo318 <camiloa0318@gmail.com>
2020-09-10 10:47:46 -0500

    Add animations using gsap Mobile navigation is out the header Styles applied for Autoalpha: 0

--- index.js
+++ index.js
@@ -2,6 +2,41 @@ const closeBtn = document.querySelector('#closeBtn')
 const openBtn = document.querySelector('#openBtn')
 const mobileMenu = document.querySelector('#mobileMenu')
 
+const bannerTl = gsap.timeline()
+const bannerH1 = document.querySelector('.banner__content h1')
+const bannerP = document.querySelector('.banner__content p')
+const bannerDiv = document.querySelector('.banner__content div')
+
+
+bannerTl.from('.header__container', {
+    duration: 1,
+    autoAlpha: 0,
+    opacity: 0,
+    y: 30,
+    ease: 'power3.inOut'
+})
+.from([bannerH1, bannerP, bannerDiv], {
+    delay: -0.4,
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    ease: 'power3.out',
+    autoAlpha: 0,
+    stagger: 0.2
+})
+.from(['.banner__image', '.banner__card'], {
+    delay: -1,
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    ease: 'power3.out',
+    autoAlpha: 0,
+    stagger: {
+        amount: 0.2
+    }
+
+})
+
 
 closeBtn.addEventListener('click', toggleMenu)
 openBtn.addEventListener('click', toggleMenu)
